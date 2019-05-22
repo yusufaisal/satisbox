2cab33019ffef2a774d06462edbe4029d420fd55
Faisal <yusufaisal9@gmail.com>
2019-05-22 08:10:40 +0700

    Initial Commit

---
+++ StatisBox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StatisBox
+//
+//  Created by iSal on 22/05/19.
+//  Copyright © 2019 iSal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
